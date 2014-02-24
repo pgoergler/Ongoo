1641e9694a63c2c525c2061e0822969f27957b14
pgoergler <pgoergler@gmail.com>
2014-02-24 10:08:45 +0400

    Add some functions
    Add : \ip()
    Add : \client_id() which return $app['client_id']() or \ip()
    Add: \whoami() which return the session user
    Add: \me() which return the session user or the \client_id()
    Add: \groupby($array, $groupby, $notexists) which return $array group
    by the key $groupby

--- src/Ongoo/Silex/functions.php
+++ src/Ongoo/Silex/functions.php
@@ -3,6 +3,79 @@
 namespace
 {
 
+    function ip()
+    {
+        if (isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        {
+            return $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } elseif (isset($_SERVER['HTTP_CLIENT_IP']))
+        {
+            return $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (isset($_SERVER['REMOTE_ADDR']))
+        {
+            return $_SERVER['REMOTE_ADDR'];
+        } elseif (isset($_SERVER['TERM']))
+        {
+            return $_SERVER['TERM'];
+        }
+        return '0.0.0.0';
+    }
+
+    function client_id()
+    {
+        $dependencyInjection = \Ongoo\Core\Configuration::getInstance()->get('application');
+        if (!$dependencyInjection->OffsetExists('client_id'))
+        {
+            $dependencyInjection['client_id'] = \ip();
+        }
+        return $dependencyInjection['client_id'];
+    }
+
+    function me()
+    {
+        $me = \whoami();
+        return $me ? $me->getLogin() : \client_id();
+    }
+
+    function getOngooUser()
+    {
+        return \me();
+    }
+
+    function whoami()
+    {
+        $dependencyInjection = \Ongoo\Core\Configuration::getInstance()->get('application');
+        if (!$dependencyInjection->OffsetExists('whoami'))
+        {
+            $dependencyInjection['whoami'] = $dependencyInjection->protect(function() use (&$dependencyInjection)
+                    {
+                        if ($dependencyInjection->OffsetExists('session') && $dependencyInjection['session'] instanceof \Quartz\QuartzGuard\Session && $dependencyInjection['session']->getGuardUser())
+                        {
+                            return $dependencyInjection['session']->getGuardUser();
+                        }
+                        return null;
+                    });
+        }
+        return $dependencyInjection['whoami']();
+    }
+
+    function groupby($array, $groupby, $notexists = '-')
+    {
+        $result = array();
+        foreach ($array as $key => $data)
+        {
+            $id = isset($data[$groupby]) ? $data[$groupby] : $notexists;
+            if (isset($result[$id]))
+            {
+                $result[$id][$key] = $data;
+            } else
+            {
+                $result[$id] = array($key => $data);
+            }
+        }
+        return $result;
+    }
+
     /**
      *
      * @param Mixed $datetime
@@ -58,7 +131,7 @@ namespace
     function trans($id, $values = array(), $domain = null, $locale = null)
     {
         $dependencyInjection = \Ongoo\Core\Configuration::getInstance()->get('application');
-        if( !$dependencyInjection->offsetExists('translator'))
+        if (!$dependencyInjection->offsetExists('translator'))
         {
             return $id;
         }
@@ -68,7 +141,7 @@ namespace
     function transChoice($id, $number, $values = array(), $domain = null, $locale = null)
     {
         $dependencyInjection = \Ongoo\Core\Configuration::getInstance()->get('application');
-        if( !$dependencyInjection->offsetExists('translator'))
+        if (!$dependencyInjection->offsetExists('translator'))
         {
             return $id;
         }
